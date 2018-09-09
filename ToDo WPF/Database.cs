0f131b0c8e8912674794f20682bf2482edc2e16e
NicholasDollick <nidollick@gmail.com>
2018-09-08 17:17:59 -0700

    test

---
+++ ToDo WPF/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace ToDo_WPF
+{
+    class Database
+    {
+        static void main(string [] args)
+        {
+            var client = new MongoClient();
+            var database = client.GetDatabase("startup");
+            var collect = database.GetCollection<BsonDocument>("employee");
+        }
+    }
+}
